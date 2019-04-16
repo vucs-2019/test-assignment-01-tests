cf1c4fb1827a44fa1fb78b89ac26a89f4c042ad0
PercyODI <pearse.2008@gmail.com>
2019-04-16 13:02:32 -0500

    Added Working Tests

--- test/test.js
+++ test/test.js
@@ -1,9 +1,56 @@
-var assert = require('assert');
+var chai = require("chai");
+var Nightmare = require('nightmare');
+var express = require('express');
 
-describe("Array", function () {
-    describe("#indexOf()", function () {
-        it("should return -1 when the value is not present", function () {
-            assert.notEqual([1, 2, 3].indexOf(4), -1);
+var should = chai.should();
+
+describe("Load A Page", function () {
+    this.timeout('5s');
+
+    var app = express();
+    var port = 8123;
+    var appServer;
+
+    app.use(express.static("../"))
+
+    let nightmare = null
+    beforeEach(() => {
+        nightmare = new Nightmare()
+    });
+
+    beforeEach(function (done) {
+        console.log("Starting Server");
+        appServer = app.listen(port, done);
+    });
+
+    afterEach(function (done) {
+        console.log("Stopping Server")
+        appServer.close(done);
+    });
+
+
+    describe('/ (Test Page)', () => {
+        it('should load without error', done => {
+            nightmare.goto('http://localhost:8123/test.html')
+                .end()
+                .then(function (result) { done() })
+                .catch(done)
+        });
+
+
+        it('should not include Hey you, add some HTML here!', function (done) {
+            nightmare
+                .goto('http://localhost:8123/test.html')
+                .wait('body')
+                .evaluate(function () {
+                    return document.body.innerHTML;
+                })
+                .end()
+                .then(function (innerHtml) {
+                    innerHtml.should.not.include("Hey you, add some HTML here");
+                    done();
+                })
+                .catch(done)
         });
     });
 });
